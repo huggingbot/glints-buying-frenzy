c0bee39828560e694a0c850044a3c8aa1eaa24d2
Wee <83656073+huggingbot@users.noreply.github.com>
2022-09-29 12:07:04 +0800

    refactor: Use http status as constants;

--- src/routers/debug.routes.ts
+++ src/routers/debug.routes.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import { apiConfig } from '~/core/api.config'
 import { appConfig } from '~/core/app.config'
 import { getCurrentSGTime } from '~/utils/dateUtil'
+import HttpStatus from 'http-status-codes'
 
 const debugRouter = express.Router()
 
 debugRouter.get('/version', (_, res) => {
-  res.status(200).json({
+  res.status(HttpStatus.OK).json({
     appVersion: appConfig.version,
     apiVersion: apiConfig.version,
     env: process.env.NODE_ENV,
