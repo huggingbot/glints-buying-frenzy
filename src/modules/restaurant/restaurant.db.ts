d50c2c084d69d0ca870decfb3699d6b202208b0c
Wee <83656073+huggingbot@users.noreply.github.com>
2022-10-02 17:16:59 +0800

    fix: Rename restaurant class name;

--- src/modules/restaurant/restaurant.db.ts
+++ src/modules/restaurant/restaurant.db.ts
@@ -2,7 +2,7 @@ import { BaseDb } from '~/core/base.db'
 import { ILogContext } from '~/core/types'
 import { IRestaurantModelAttrs, RestaurantModelStatic, restaurantModelStatic } from '~/models/RestaurantModel'
 
-export class RestaurantServiceDb extends BaseDb<RestaurantModelStatic, IRestaurantModelAttrs> {
+export class RestaurantDb extends BaseDb<RestaurantModelStatic, IRestaurantModelAttrs> {
   public constructor(logContext: ILogContext) {
     super(logContext, restaurantModelStatic)
   }
